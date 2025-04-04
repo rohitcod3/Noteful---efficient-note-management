c22c697ed31951c36df9b7f9574debca956e5d9e
rohitcod3 <rohit.bangar9@gmail.com>
2025-04-04 09:22:26 +0530

    Setup routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Create from './components/Create'
+import Navbar from './components/Navbar'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import NoteDetails from './components/NoteDetails'
+import Edit from './components/Edit'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <BrowserRouter>
+     <Navbar/>
+
+     <div className=''>
+     <Routes>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/create' element={<Create/>}/>
+     <Route path="/note/:id" element={<NoteDetails/>}/>
+     <Route path="/edit/:id" element={<Edit/>} />
+     </Routes>
+     </div>
+     <ReactQueryDevtools initialIsOpen={false}/>
+     </BrowserRouter>
+    
   )
 }
 
